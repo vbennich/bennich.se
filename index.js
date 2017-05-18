f5937726867e54ab0f54377d50eeb64b7f7342cc
Victor Bennich <vicbe572@student.liu.se>
2017-05-18 20:46:22 +0200

    altered log

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Hello Http');
+  res.end('Hello there my friend');
 });
 server.listen(8080);
