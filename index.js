e426f05d5ef4a934d7154b573d86d1a3b1217d65
Victor Bennich <vicbe572@student.liu.se>
2017-05-18 21:42:34 +0200

    changed welcome message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Hello there my friend');
+  res.end('My first heroku app');
 });
 console.log(process.env.PORT);
 server.listen(process.env.PORT);
