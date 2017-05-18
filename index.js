d55f6d034959cebd976d6cdb088eede1dac1b4cb
Victor Bennich <vicbe572@student.liu.se>
2017-05-18 22:56:42 +0200

    added use for static content

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ var path = require('path');
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-
+app.use(express.static('assets'));
+app.use(express.static('images'));
 app.listen(process.env.PORT);
